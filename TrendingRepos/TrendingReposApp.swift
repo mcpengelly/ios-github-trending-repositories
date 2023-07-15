7a59124a4b0a8abb20688892cfa6cc673d20d3a4
matt pengelly <pengelly.mat@gmail.com>
2023-07-15 14:23:25 -0400

    update comments

--- TrendingRepos/TrendingReposApp.swift
+++ TrendingRepos/TrendingReposApp.swift
@@ -9,38 +9,53 @@
 // - verify accessibility with physical device
 // - final pass physical device test:
     // all tabs, all translations, accessibility mode on/off, dark mode persisting state, loggout and loggedin states
+// - rearrange things into folders
+// - make repo public
 // - launch details
     // - register for apple developer program 99$ USD
     // https://developer.apple.com/programs/enroll/
     // - see app store review guidelines
-    // - testflight?
+    // - testflight? how?
     // - app assets:
-        // title: Trending Github Projects (ios)
+        // title: Trending Github Projects (ios) - ensure github app has the same name
         // description: Keep up to date with the latest daily, weekly & monthly trending code repositories straight from the source!
         // keywords: github, trending, code, respository, latest, popular, discovery, tools
-        // demo: check slack for vid - refilm demo after latest features
+        // demo: check slack for vid - refilm demo after latest features - refilm demo
         // Web APIs used:
-        // 1. unofficial github trending repositories: https://github.com/alisoft/github-trending-api
-        // 2. official github OAuth, star/unstar repositories on behalf of the user & checking users star status
+        // 1. Unofficial github trending repositories: https://github.com/alisoft/github-trending-api
+        // 2. Official github OAuth, star/unstar repositories on behalf of the user & checking users star status
         // Native APIs:
-            // IOS Keychain
-                // fields: githubAccessToken & darkModeEnabled
-        // Secrets.plist, to fork/work on the repo you need this setup.
-            // CLIENT_ID and CLIENT_SECRET
+            // IOS Keychain:
+                // fields: githubAccessToken
+            // UserDefaults:
+                // fields: darkModeEnabled
+        // Secrets.plist (to fork/work on the repo you need this setup, you can make your own oauth app)
+            // fields: CLIENT_ID & CLIENT_SECRET
+        // Features:
+            // Finds Trending Projects: Keep up to date with the latest code projects on github at a glance
+            // Github Oauth Integeration: Star repositories as though you were on Github.com
+            // Dark Mode: Protect your eyes by toggling darkmod within the app. Defaults to your ios setting
+            // Open Source: the code is open to everyone and will remain so
+            // A11y Compliant: Supports IOS screenreader
+            // Internationalization: Supports english, french, german, italian, portugeuse, spanish. If you want more just raise an Issue with the requested language.
+                
     // - expect apple review to take 3 business days
     // - upload an archived app file to app store connect - how to generate archived app when time comes?
     // - release automatically or manually (you choose)
 
 // demo 2 target:
-// - buy me a coffee button
-// - unit testing the most important parts: in progress
-// - convert to futures instead of callbacks
-// - filter on programming language if one specified, otherwise all. always default back to all on launch.
-// - introduce abstractions over ThreadQueue.main.dataTask in NetworkManager?
-// - TokenManager does too much, exclude the parts that have to do with GithubAPI
-// - use result wherever applicable
+// - unit test the most important parts - should be done before refactor, that way we know if the refactor broke anything
 // - address all TODOS
-// - nav bar hides when reaching the bottom - couldnt fix when i tried
+// - convert completion handlers to futures
+// - filter on programming language if one specified, otherwise all. always default back to all on launch.
+// - TokenManager does too much, exclude the parts that have to do with GithubAPI
+// - use result wherever applicable to wrap the response value, that way the caller can switch on failure if need.
+// - introduce abstractions over ThreadQueue.main.dataTask in NetworkManager?
+// - coffee?
+
+// known bugs - wont fix:
+    // - nav bar hides when reaching the bottom
+    // - nav bar hides when transitioning screens
 
 import SwiftUI
 
